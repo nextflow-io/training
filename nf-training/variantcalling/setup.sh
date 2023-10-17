d7f1b3fdba92f7e339a749297104ad806939d73f
Francesco L <francesco.lescai@unipv.it>
2023-10-17 12:10:35 +0000

    attempt fixing snpeff issues with cache

--- nf-training/variantcalling/setup.sh
+++ nf-training/variantcalling/setup.sh
@@ -16,9 +16,8 @@ nextflow run nf-core/sarek \
 nextflow run nf-core/sarek \
 --input /workspace/gitpod/nf-training/data/reads/variantcalling/sarek-input.csv \
 --outdir . \
---tools haplotypecaller \
+--tools haplotypecaller,snpeff \
 --genome GRCh38chr21 \
---skip_tools haplotypecaller_filter \
 --joint_germline \
 --intervals /workspace/gitpod/nf-training/variantcalling/chr21_intervals.list \
 -resume
