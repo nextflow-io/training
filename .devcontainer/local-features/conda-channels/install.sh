eb23a315bc4fa4a9fc1db4ea3fb9bbf1e2b20b0f
Ken Brewer <kenibrewer@users.noreply.github.com>
2025-05-10 10:51:37 -0400

    devcontainer: install needed cli tools using uv

--- .devcontainer/local-features/conda-channels/install.sh
+++ .devcontainer/local-features/conda-channels/install.sh
@@ -5,12 +5,3 @@
 conda config --add channels bioconda
 conda config --add channels conda-forge
 conda config --set channel_priority strict
-conda update --quiet --yes --all
-
-conda install --quiet --yes --name base \
-    nf-core \
-    pre-commit \
-    linkify-it-py \
-    pytest-workflow
-
-conda clean --all --force-pkgs-dirs --yes
