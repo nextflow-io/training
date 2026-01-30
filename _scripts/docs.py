e1536db405e038e80e594c6c7776d79cfb1a8ba7
Phil Ewels <phil.ewels@seqera.io>
2026-01-30 17:14:35 +0100

    Bump mkdocs-quiz minimum version to 1.5.4 for language detection fix

--- _scripts/docs.py
+++ _scripts/docs.py
@@ -10,7 +10,7 @@
 #     "mkdocs-material",
 #     "pymdown-extensions>=10.12",
 #     "mkdocs-enumerate-headings-plugin>=0.6.0",
-#     "mkdocs-quiz>=1.5.0",
+#     "mkdocs-quiz>=1.5.4",
 #     "mike",
 # ]
 # ///
